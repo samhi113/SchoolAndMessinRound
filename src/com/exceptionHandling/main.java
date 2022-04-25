312de18f99accb3015398e2d5c0df1821b832da7
samhi113 <samhiggins113@gmail.com>
2022-04-25 11:39:39 -0400

    Finished Exception Handling

--- src/com/exceptionHandling/main.java
+++ src/com/exceptionHandling/main.java
@@ -1,13 +1,17 @@
 package com.exceptionHandling;
 
+import java.util.Random;
+
 public class main {
 
     public static void main(String[] args) {
+        Random rand = new Random();
         try{
-            int a = 10;
-            int b = 0;
+            int a = rand.nextInt(20);
+            int b = rand.nextInt(5);
             int c = a/b;
-            System.out.println(c);
+            int r = a%b;
+            System.out.println(a + " / " + b + " = " + c + " remainder " + r);
         }
         catch(Exception e){
             System.out.println("ERROR:\n" + e.getMessage());
